db0b5731e3527b0aa623cb297c171861b921cb4a
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-03-12 16:07:07 -0300

    Add Google Fonts and StatusBar to App component

--- App.tsx
+++ App.tsx
@@ -1,9 +1,35 @@
-import { Text, View } from "react-native";
+import { StatusBar, Text, View } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null; // or any loading indicator
+  }
+
   return (
-    <View>
-      <Text>hi</Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#202024",
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        translucent={true}
+        backgroundColor="transparent"
+      />
+      <Text style={{ fontFamily: "Roboto_700Bold" }}>hi</Text>
     </View>
   );
 }
