0abc7b3d29585c4fceb894b75f0b0a71a0de7854
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-03-15 14:24:53 -0300

    Add SVG support and update Signin screen

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,6 +1,6 @@
-import { VStack, Image } from "native-base";
+import { VStack, Image, Text, Center, Heading } from "native-base";
 import BackgroundImg from "@assets/background.png";
-
+import LogoSvg from "@assets/logo.svg";
 export default function SignIn() {
   return (
     <VStack flex={1} bg="gray.700">
@@ -10,6 +10,17 @@ export default function SignIn() {
         resizeMode="contain"
         position="absolute"
       />
+      <Center my={24}>
+        <LogoSvg />
+        <Text color="gray.100" fontSize="sm">
+          Treine sua mente e o seu corpo
+        </Text>
+      </Center>
+      <Center>
+        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+          Acesse sua conta
+        </Heading>
+      </Center>
     </VStack>
   );
 }
