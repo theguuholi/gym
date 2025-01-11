89284f7b85d25d0e8fdc2a8b0304fcdbd97960d1
Gus <g.92oliveira@gmail.com>
2025-01-11 07:21:37 -0300

    add background image

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,18 @@
-import { VStack } from "@gluestack-ui/themed"
+import { VStack, Image } from "@gluestack-ui/themed"
+import BackgrounImage from "@assets/background.png";
 
 const SignIn = () => {
     return (
-       <VStack flex={1} bg='$gray700' >
-
-       </VStack>
+        <VStack flex={1} bg='$gray700' >
+            <Image
+                w="$full"
+                h={624}
+                source={BackgrounImage}
+                defaultSource={BackgrounImage}
+                alt="People Training"
+                position="absolute"
+            />
+        </VStack>
     )
 }
 export default SignIn
