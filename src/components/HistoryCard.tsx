481c1180fe5415d14ca6b4bef8e84aa855c72814
Gus <g.92oliveira@gmail.com>
2025-01-13 15:31:47 -0300

    history card

---
+++ src/components/HistoryCard.tsx
@@ -0,0 +1,29 @@
+import { HStack, VStack, Heading, Text } from "@gluestack-ui/themed"
+
+const HistoryCard = () => {
+    return (
+        <HStack w="$full" px="$4" py="$4" mb="$3" bg="$gray600" rounded="$md" alignItems="center" justifyContent="space-between">
+            <VStack mr="$5">
+                <Heading
+                    color="$white"
+                    fontSize="$md"
+                    textTransform="capitalize"
+                    fontFamily="$heading"
+                >Back</Heading>
+                <Text
+                    color="$gray100"
+                    fontSize="$lg"
+                    numberOfLines={1}
+                >Front </Text>
+            </VStack>
+            <Text
+                color="$gray300"
+                fontSize="$md"
+            >
+                08:56
+            </Text>
+        </HStack>
+    )
+}
+
+export default HistoryCard;
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,26 @@
-import { Center, Text } from "@gluestack-ui/themed"
+import ScreenHeader from "@components/ScreenHeader";
+import UserPhoto from "@components/UserPhoto";
+import { Center, Text, VStack } from "@gluestack-ui/themed"
+import { ScrollView } from "react-native";
 
 
 const Profile = () => {
     return (
-        <Center>
-            <Text>Profile</Text>
-        </Center>
+        <VStack flex={1}>
+            <ScreenHeader title="Profile" />
+            <ScrollView
+                contentContainerStyle={{ paddingBottom: 32 }}
+            >
+                <Center mt="$6" px="$10">
+                    <UserPhoto
+                        source={{ uri: "https://github.com/theguuholi.png" }}
+                        alt="Profile Photo"
+                        size="xl"
+                    />
+                </Center>
+
+            </ScrollView>
+        </VStack>
     )
 }
 
