a0b114092c48e1eaa622dfc6365c5950d90d95bd
Gus <g.92oliveira@gmail.com>
2025-01-13 09:25:26 -0300

    Criando lista de treinos

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,34 @@
-import { Center, Text } from "@gluestack-ui/themed"
+import Group from "@components/Group";
+import HomeHeader from "@components/HomeHeader";
+import { VStack } from "@gluestack-ui/themed"
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 
 const Home = () => {
+    const [groups, setGroups] = useState(["Back", "Shoulder", "Triceps", "Biceps", "Chest", "Legs"]);
+    const [groupSelected, setGroupSelected] = useState("Back");
+
     return (
-        <Center>
-            <Text>Home</Text>
-        </Center>
+        <VStack flex={1}>
+            <HomeHeader></HomeHeader>
+
+            <FlatList
+                data={groups}
+                keyExtractor={(item) => item}
+                renderItem={({ item }) => (
+                    <Group
+                        name={item}
+                        isActive={groupSelected === item}
+                        onPress={() => setGroupSelected(item)}
+                    />
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{ paddingHorizontal: 32 }}
+                style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
+            />
+        </VStack>
     )
 }
 
